6e67779c6cc25cb59e09c1fc793f81c0ce278136
yaquake <ivan@programmer.net>
2020-04-05 18:15:24 +1200

    README

--- connection.js
+++ connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 
-mongoose.connect(`mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@cluster0-njnty.mongodb.net/my_database`, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(`mongodb+srv://yaquake:uVbiho60@cluster0-njnty.mongodb.net/my_database`, {useNewUrlParser: true, useUnifiedTopology: true})
 
 
 const connection = mongoose.connection
