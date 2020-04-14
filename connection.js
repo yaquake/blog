97cc7aca314cd03f8c3dedf3343db9cc59e78bd8
yaquake <ivan@programmer.net>
2020-04-14 20:27:39 +1200

    connection

--- connection.js
+++ connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 
-mongoose.connect(`mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}60@cluster0-njnty.mongodb.net/my_database`, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(`mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0-njnty.mongodb.net/my_database`, {useNewUrlParser: true, useUnifiedTopology: true})
 mongoose.set('useCreateIndex', true)
 
 const connection = mongoose.connection
